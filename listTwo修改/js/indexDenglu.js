35aefefba221c63e292774b3cf517225ddb76580
cangtiana3 <7806913+cangtiana3@user.noreply.gitee.com>
2020-07-15 10:04:57 +0800

    V联网

---
+++ listTwo修改/js/indexDenglu.js
@@ -0,0 +1,55 @@
+$(function(){
+	/*
+	 ***********登录页js开始********
+	*/
+	//文本框获得焦点
+	$(".inputTxt") .each(function(){
+     var thisVal=$(this).val();
+     //判断文本框的值是否为空，有值的情况就隐藏提示语，没有值就显示
+     if(thisVal!=""){
+       $(this).siblings("label").hide();
+      }else{
+       $(this).siblings("label").show();
+      }
+     //聚焦型输入框验证 
+     $(this).focus(function(){
+       $(this).siblings("label").hide();
+      }).blur(function(){
+        var val=$(this).val();
+        if(val!=""){
+         $(this).siblings("label").hide();
+        }else{
+         $(this).siblings("label").show();
+        } 
+      });
+	});
+	//获得焦点文本框变亮
+	$(".inputTxt").focus(function(){
+		$(this).css({"border-color":"#f29120","color":"#000"})
+	});
+	//失去焦点变暗
+	$(".inputTxt").blur(function(){
+		$(this).css({"border-color":"#ccc"})
+	});
+	//选中记住密码
+	$(".rememberClick").click(function(){
+		$(".remember").toggleClass("blue");
+	});
+	
+	/****************************
+	 ***********列表页js开始********
+	* ***************************/
+	//选中排序样式变化
+	$(".CommodityList").click(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+	});
+	//排序箭头变化
+	$(".sanjiao a").click(function(){
+		$(this).addClass("red").siblings().removeClass("red");
+	});
+	//分页样式变化
+	$(".onePage").click(function(){
+		$(this).addClass("allPageActive").siblings().removeClass("allPageActive");
+	});	
+	
+});
---
+++ 个人中心/js/personalCenter.js
@@ -0,0 +1,79 @@
+// JavaScript Document
+//选项卡切换调用tabClick
+function tabClick(tabTit, tabClass, tabTxt) {
+	$(tabTit).click(function() {
+		var index = $(this).index();
+		$(tabTit).removeClass(tabClass);
+		$(tabTxt).css("display", "none");
+		$(this).addClass(tabClass);
+		$(tabTxt).eq(index).css("display", "block");
+	});
+}	
+
+//文本框获得焦点
+$(".inputTxt") .each(function(){
+ var thisVal=$(this).val();
+ //判断文本框的值是否为空，有值的情况就隐藏提示语，没有值就显示
+ if(thisVal!=""){
+   $(this).siblings("label").hide();
+  }else{
+   $(this).siblings("label").show();
+  }
+ //聚焦型输入框验证 
+ $(this).focus(function(){
+   $(this).siblings("label").hide();
+  }).blur(function(){
+	var val=$(this).val();
+	if(val!=""){
+	 $(this).siblings("label").hide();
+	}else{
+	 $(this).siblings("label").show();
+	} 
+  });
+});	
+$(function(){
+	//点击 - 收起下拉菜单
+	var Index;
+	$(".myDeal").click(function(){
+		Index = $(".myDeal").index(this);
+		$(this).toggleClass("on");
+		if( $(this).hasClass("on") ){
+			$(this).find("i").html(" + ");
+			//$(this).removeClass("on")
+			$(".myDealList").eq(Index).slideUp();
+		}else{
+			$(this).find("i").html(" - ");			
+			$(".myDealList").eq(Index).slideDown();
+		}
+	});
+	
+	//点击变背景
+	tabClick(".orderList", "active", ".fullOrderUl");
+	
+	//点击单选文本变背景
+	$(".radioOne").click(function(){
+		$(this).addClass("checked").siblings().removeClass("checked")
+	});
+	
+	//点击 + 号 数值增加
+		var Num;
+		$(".add").click(function(){
+			var Num =parseInt($(".number").val());
+			if($(".number").val() == Num){
+				alert("不能大于商品总量！")
+			}else{
+				var addNum = parseInt(Num + 1);
+			}
+		});
+		$(".ease").click(function(){
+			var Num =parseInt($(".number").val());
+			var easeNum = parseInt(Num -1);
+			
+			if($(".number").val() == 0){
+				$(".number").val() == 0
+				alert("当前没有商品！")
+			}else{
+				$(".number").val(easeNum);
+			}
+		});
+});
